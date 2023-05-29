17e04f4e3024f606aea688e678a9e476cf198700
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-29 08:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+let element = document.getElementsByClassName("code");
+
+for(let i=0; i<element.length; i++){
+	element[i].addEventListener("keyup", (event) =>{
+		let currentElement = event.target
+			let code = event.key.charCodeAt(0);
+		if(code>= 48 && code <=57){
+			let nextElemet = currentElement.nextElementSibling;
+			if(nextElemet){
+				nextElemet.focus();
+			}
+			
+		}
+	})
+}
